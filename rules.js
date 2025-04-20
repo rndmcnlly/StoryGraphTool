7add7a37538c6ace230aafdc3b83da64a9aaef2a
BellaTheAlien <161175405+BellaTheAlien@users.noreply.github.com>
2025-04-20 16:18:35 -0700

    finished story and code

--- rules.js
+++ rules.js
@@ -1,34 +1,65 @@
 class Start extends Scene {
     create() {
-        this.engine.setTitle("Title goes here"); // TODO: replace this text using this.engine.storyData to find the story title
+        this.engine.setTitle(this.engine.storyData.Title); //this.engine.storyData to find the story title
         this.engine.addChoice("Begin the story");
+
+        this.engine.keyItem = false;
+        this.engine.lockItem = false;
     }
 
     handleChoice() {
-        this.engine.gotoScene(Location, "Home"); // TODO: replace this text by the initial location of the story
+        this.engine.gotoScene(Location, this.engine.storyData.InitialLocation); //the initial location of the story, goes to the scene that is in the choice
     }
 }
 
 class Location extends Scene {
     create(key) {
-        let locationData = undefined; // TODO: use `key` to get the data object for the current story location
-        this.engine.show("Body text goes here"); // TODO: replace this text by the Body of the location data
-        
-        if(true) { // TODO: check if the location has any Choices
-            for(let choice of ["example data"]) { // TODO: loop over the location's Choices
-                this.engine.addChoice("action text"); // TODO: use the Text of the choice
-                // TODO: add a useful second argument to addChoice so that the current code of handleChoice below works
+        //
+        //current mess - the "The End" buttion shows in the "Door" scene. fix: unknown
+        //
+        let locationData = this.engine.storyData.Locations[key]; //`key` to get the data object for the current story location
+        this.engine.show(locationData.Body); //shows the body text of each scene
+
+        //check: at the door scean if the key item and the lock item are true go to the next area
+        //else = when the player has nothing and can't open the door
+        if(key == "Try"){
+            if(this.engine.keyItem == true && this.engine.lockItem == true){
+                this.engine.gotoScene(Location, "Puzzle");
             }
+            else{
+                this.engine.show("You don't have the right items with you");
+                this.engine.gotoScene(Location, "Door");
+            }
+
+        }
+        
+        if(locationData.Choices && locationData.Choices.length > 0) { //checks if the location has any Choices
+            for(let choice of locationData.Choices) { //loop over the location's Choices
+                this.engine.addChoice(choice.Text, choice); //use the Text of the choice
+            }
+
+            
         } else {
             this.engine.addChoice("The end.")
         }
+        
     }
 
     handleChoice(choice) {
         if(choice) {
+
+            //when in the right scene, change the item to ture - means that the item was collected
+            if(choice.Target == "Pick Key"){
+                this.engine.keyItem = true;
+            }
+            if(choice.Target == "Pick Lock"){
+                this.engine.lockItem = true;
+            }
+
             this.engine.show("&gt; "+choice.Text);
             this.engine.gotoScene(Location, choice.Target);
-        } else {
+        } 
+        else {
             this.engine.gotoScene(End);
         }
     }
